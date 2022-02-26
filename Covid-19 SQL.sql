bd9677c524a3539ef9821bd0541ec5e443494698
Pani Vignesh <86531004+Pani-takraw@users.noreply.github.com>
2022-02-26 21:33:28 +0530

    Update Covid-19 SQL.sql

--- Covid-19 SQL.sql
+++ Covid-19 SQL.sql
@@ -1,11 +1,12 @@
+-- Querying full Data and ordering by 3 & 4th column
 Select * From PortfolioProjects..CovidDeaths
 Order By 3,4
 
+-- Querying full Data and ordering by 3 & 4th column
+Select * From PortfolioProjects..CovidVaccinations$
+Order By 3,4
 
---Select * From PortfolioProjects..CovidVaccinations$
---Order By 3,4
-
-
+-- Querying specific columns
 Select location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProjects..CovidDeaths$
 order by 1,2
@@ -18,8 +19,8 @@ Where location LIKE '%India%'
 order by 1,2
 
 
---Now we are looking at Total cases vs population
--- Shows what percentage of population got covid
+--Now we are looking at Total cases vs population 
+-- Shows what percentage of population got covid f=grouping by location
 Select location, date, population, total_cases,  (total_cases/population)*100 as CovidaffectedPercentage
 From PortfolioProjects..CovidDeaths$
 Where location LIKE '%India%'
@@ -32,16 +33,17 @@ From PortfolioProjects..CovidDeaths$
 Group by location, population
 order by Percentperpopulation DESC
 
--- Showing countries with highest death count per population
-Select location, MAX(Cast(total_deaths as int)) as Totaldeathcount
-From PortfolioProjects..CovidDeaths$
-Where continent is not null
-Group by location, population
-order by Totaldeathcount DESC
 
 -- We found that there are few locations which are named after continents so we are excluding them for now
 Select * From PortfolioProjects..CovidDeaths
 Where continent is not null
+
+-- Showing countries with highest death count per population
+Select location, MAX(Cast(total_deaths as int)) as Totaldeathcount    # We are using casting because some of the rows of total_deadths has "null" values
+From PortfolioProjects..CovidDeaths$
+Where continent is not null					     
+Group by location, population
+order by Totaldeathcount DESC
 
 -- Breaking things by continent
 Select continent, MAX(Cast(total_deaths as int)) as Totaldeathcount
@@ -50,7 +52,7 @@ Where continent is not null
 Group by continent
 order by Totaldeathcount DESC
 
--- These are the correct number of deadths
+-- These are the correct number of deadths after excluding continents with "Null" Values
 Select location, MAX(Cast(total_deaths as int)) as Totaldeathcount
 From PortfolioProjects..CovidDeaths$
 Where continent is null
@@ -71,7 +73,7 @@ Where continent is not null
 order by 1,2
 
 
--- Covid Vaccination
+-- Looking at both Covid Deadths and Covid Vaccination by Joining them by location and date columns 
 Select * From PortfolioProjects..CovidDeaths dea
 JOIN PortfolioProjects..CovidVaccinations$ vac
 	ON dea.location = vac. location
@@ -87,7 +89,7 @@ Where dea.continent is not null
 Group by dea.continent,dea.location,dea.date, dea.population, vac.new_vaccinations
 order by 2,3
 
--- Using CTE
+-- Using CTE (Common table expressions)
 
 WITH PopvsVac (Continent, location, date, population, new_vaccination, Rollingpeoplevaccinated)
 AS
@@ -115,10 +117,11 @@ Where dea.continent is not null AND dea.location = 'India'
 --Group by dea.continent,dea.location,dea.date, dea.population, vac.new_vaccinations
 )
 SELECT *, (Rollingpeoplevaccinated/population)*100 FROM PopvsVac1
+-- CTE can be used to make calculations (Rollingpeoplevaccinated/population)*100 with calculated columns in the select statement(Rollingpeoplevaccinated)
 
 
 
--- Creating View
+-- Creating Views for future visualizations
 Create View PercentPopulationVaccinated as
 Select dea.continent,dea.location,dea.date, dea.population, vac.new_vaccinations,
 SUM(CAST(vac.new_vaccinations as bigint)) OVER (Partition by dea.location Order by dea.location, dea.date) as Rollingpeoplevaccinated From PortfolioProjects..CovidDeaths dea
