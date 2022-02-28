d3a7263a2c4241d10fd78cad2db7d4a42d6b3901
Pani Vignesh <86531004+Pani-takraw@users.noreply.github.com>
2022-02-28 16:49:01 +0530

    Update Create and update table.sql

--- Create and update table.sql
+++ Create and update table.sql
@@ -1,3 +1,4 @@
+-- Creating a table "EmployeeDetails"
 Create Table EmployeeDetails (
 EmployeeID int,
 FirstName Varchar(100),
@@ -5,11 +6,17 @@ LastName Varchar(100),
 Age int
 )
 
-
+-- Inserting Data into "EmployeeDetails"
 Insert Into EmployeeDetails Values 
 (1001, 'Pani', 'Vignesh', 27),
 (1002, 'Elango', 'Padmanaban', 25),
 (1003, 'Yukesh', 'Nagarajan', 25),
 (1004, 'Vignesh','Ramesh', 28)
 
+-- Querying Table EmployeeDetails which we have just created
+
 Select * From EmployeeDetails
+
+Select EmployeeID, FirstName From EmployeeDetails
+
+Select FirstName, LastName, Age From EmployeeDetails
