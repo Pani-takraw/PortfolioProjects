b833aff4f39a29579bb0fed5d53e6ddad941b762
Pani Vignesh <86531004+Pani-takraw@users.noreply.github.com>
2022-02-28 16:46:41 +0530

    Add files via upload

---
+++ Create and update table.sql
@@ -0,0 +1,15 @@
+Create Table EmployeeDetails (
+EmployeeID int,
+FirstName Varchar(100),
+LastName Varchar(100),
+Age int
+)
+
+
+Insert Into EmployeeDetails Values 
+(1001, 'Pani', 'Vignesh', 27),
+(1002, 'Elango', 'Padmanaban', 25),
+(1003, 'Yukesh', 'Nagarajan', 25),
+(1004, 'Vignesh','Ramesh', 28)
+
+Select * From EmployeeDetails
